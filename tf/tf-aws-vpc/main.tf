71ad563214e7c402a72f18ec3dd97c86a6f03cdc
Adam Scharf <scharfat@gmail.com>
2018-08-27 20:03:01 -0400

    Add private ec2 behind a private sg and nat gw

--- tf/tf-aws-vpc/main.tf
+++ tf/tf-aws-vpc/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block                       = "10.0.0.0/16"
   assign_generated_ipv6_cidr_block = true
+  enable_dns_hostnames             = true
 
   tags = "${merge(
     var.tags,
@@ -86,6 +87,43 @@ resource "aws_security_group" "web" {
   )}"
 }
 
+resource "aws_security_group" "private" {
+  name        = "Private SG"
+  description = "Allow private inbound traffic for SSH and egress via NAT GW"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    description     = "Private SSH"
+    security_groups = ["${aws_security_group.web.id}"]
+  }
+
+  ingress {
+    from_port       = "-1"
+    to_port         = "-1"
+    protocol        = "icmp"
+    description     = "All private ICMP"
+    security_groups = ["${aws_security_group.web.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    description = "All Outbound Traffic"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${merge(
+    var.tags,
+    map(
+        "Name", "Private SG"
+    )
+  )}"
+}
+
 resource "aws_internet_gateway" "main" {
   vpc_id = "${aws_vpc.main.id}"
 
@@ -93,6 +131,30 @@ resource "aws_internet_gateway" "main" {
     var.tags,
     map(
         "Name", "Main IGW"
+    )
+  )}"
+}
+
+resource "aws_eip" "nat_gw" {
+  depends_on = ["aws_internet_gateway.main"]
+  vpc        = true
+
+  tags = "${merge(
+    var.tags,
+    map(
+        "Name", "NAT GW Elastic IP"
+    )
+  )}"
+}
+
+resource "aws_nat_gateway" "gw" {
+  subnet_id     = "${aws_subnet.public.id}"
+  allocation_id = "${aws_eip.nat_gw.id}"
+
+  tags = "${merge(
+    var.tags,
+    map(
+        "Name", "NAT GW"
     )
   )}"
 }
@@ -109,6 +171,24 @@ resource "aws_route_table" "public" {
     var.tags,
     map(
         "Name", "Public Route Table"
+    )
+  )}"
+
+  depends_on = ["aws_internet_gateway.main"]
+}
+
+resource "aws_default_route_table" "default" {
+  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.gw.id}"
+  }
+
+  tags = "${merge(
+    var.tags,
+    map(
+        "Name", "Default Route Table"
     )
   )}"
 
@@ -150,23 +230,17 @@ module "ec2_public" {
   # depends_on = ["aws_internet_gateway.gw"]
 }
 
-# module "ec2_private" {
-#   source = "terraform-aws-modules/ec2-instance/aws"
+module "ec2_private" {
+  source = "terraform-aws-modules/ec2-instance/aws"
 
+  name           = "Main VPC - Private"
+  instance_count = 1
 
-#   name           = "Default VPC Instance"
-#   instance_count = 1
+  ami                    = "${data.aws_ami.amzn2.id}"
+  instance_type          = "t2.micro"
+  key_name               = "personal_aws_adamscharf"
+  vpc_security_group_ids = ["${aws_security_group.private.id}"]
+  subnet_id              = "${aws_subnet.private.id}"
 
-
-#   ami                         = "${data.aws_ami.amzn2.id}"
-#   instance_type               = "t2.micro"
-#   key_name                    = "personal_aws_adamscharf"
-#   vpc_security_group_ids      = ["${aws_security_group.web.id}"]
-#   subnet_id                   = "${aws_subnet.public.id}"
-#   associate_public_ip_address = true
-#   user_data                   = "${file("../../scripts/user-data.sh")}"
-
-
-#   tags = "${var.tags}"
-# }
-
+  tags = "${var.tags}"
+}
